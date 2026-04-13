a6405846769d32d766e874328eb62efffd523278
callum fisher <callumfisher2006@gmail.com>
2026-04-13 19:41:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import { Server } from 'socket.io';
+import express from 'express';
+import { createServer } from 'node:http';
+
+const port = process.env.PORT || 8080;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const maxClientsPerIPA = 1;
+const target = 100;    
+let clients = [];
+
+app.get('/', (req, res) => {
+	res.status(200);
+});
+
+const update = () => {
+	io.emit('u', {
+		c: clients.length,
+		t: target
+	});
+};
+
+io.on('connection', socket => {
+	let ipa = socket.handshake.headers['x-forwarded-for']?.split(',')[0].trim() || socket.handshake.address;
+	// Check limit:
+	let existing = clients.filter(c => c.ipa === ipa).length;
+	if (existing >= maxClientsPerIPA) {
+		socket.disconnect();
+		return;
+	}
+	// Add client
+	const client = { socket, ipa };
+	clients.push(client);
+	console.log(`Connected: ${ipa} | Total: ${clients.length}`);
+	updateCounter();
+	socket.on('disconnect', () => {
+		clients = clients.filter(c => c !== client);
+		updateCounter();
+		console.log(`Disconnected: ${ipa} | Total: ${clients.length}`);
+	});
+});
+
+server.listen(port, () => console.log(`live @ ${port}`));
